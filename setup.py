0157e62330b2820e1d405c07aa3fa41f0547ec47
wala-bingung <112078996+wala-bingung@users.noreply.github.com>
2022-09-20 11:45:29 +0700

    setup with python

---
+++ setup.py
@@ -0,0 +1,15 @@
+print ("input readme for see readme. input install for see download url. input error for error the system")
+print ("Python terminal")
+
+terminal = input ("github.com/wala-bingung/question-application>")
+if terminal="readme":
+  print ("click readme.md or click reaadme.html for see readme")
+  
+if terminal="install":
+  print ("click installer file to install")
+
+if terminal="error":
+  print ("system ERROR. to fix system. please go to youtube.com")
+  print ("ERROR")
+  print (terminal)
+  print (f'ERROR {terminal} system ERROR youtube.com")
